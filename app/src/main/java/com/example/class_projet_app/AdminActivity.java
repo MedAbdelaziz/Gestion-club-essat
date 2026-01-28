06dded000f178f006490b126557792042f47c987
Med Abdelaziz <medabdelaziz.benzina@gmail.com>
2026-01-28 17:20:48 +0100

    Add admin actions

--- app/src/main/java/com/example/class_projet_app/AdminActivity.java
+++ app/src/main/java/com/example/class_projet_app/AdminActivity.java
@@ -13,17 +13,41 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class AdminActivity extends AppCompatActivity {
 
-    Button members;
+    Button members,meetings,activites,pvs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
-
         members = findViewById(R.id.membersBtn);
+        meetings = findViewById(R.id.meetingsBtn);
+        activites = findViewById(R.id.activitiesBtn);
+        pvs = findViewById(R.id.pvBtn);
+
         members.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(AdminActivity.this, MembersList.class );
+                startActivity(i);
+            }
+        });
+        meetings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(AdminActivity.this, ManageMeetings.class );
+                startActivity(i);
+            }
+        });
+        activites.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(AdminActivity.this, ManageActivities.class );
+                startActivity(i);
+            }
+        });
+        pvs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(AdminActivity.this, ManagePvs.class );
                 startActivity(i);
             }
         });
